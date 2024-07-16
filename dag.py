6b01c941b4790664b8476ea8e69b755de5ab812f
swathik1996 <145492237+swathik1996@users.noreply.github.com>
2024-07-16 19:19:09 +0530

    Create dag.py

---
+++ dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.gcs_to_gcs import GCSToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.transfers.bigquery_to_gcs import BigQueryToGCSOperator
+from airflow.providers.google.cloud.transfers.bigquery_to_mysql import BigQueryToMySqlOperator
+from airflow.utils.dates import days_ago
+from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
+from airflow.providers.google.cloud.transfers.gcs_to_mysql import GCSToMySqlOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='gcs_to_cloudsql',
+    default_args=default_args,
+    description='Load data from GCS to Cloud SQL',
+    schedule_interval='@daily',
+    start_date=days_ago(1),
+    catchup=False,
+) as dag:
+
+    # Task: Load data from GCS to Cloud SQL
+    load_gcs_to_cloudsql = GCSToMySqlOperator(
+        task_id='load_gcs_to_cloudsql',
+        bucket_name='your-gcs-bucket-name',
+        source_objects=['path/to/your/file.csv'],
+        schema_fields=[
+            {'name': 'field1', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'field2', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+            # Add schema fields as per your data
+        ],
+        destination_project_dataset_table='your_project_id.your_dataset.your_table',
+        google_cloud_storage_conn_id='google_cloud_default',
+        mysql_conn_id='cloud_sql_default',
+        write_disposition='WRITE_TRUNCATE',
+    )
+
+    load_gcs_to_cloudsql
+
