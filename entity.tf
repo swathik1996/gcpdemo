02c34ba23586fce29e6b0e7f12239b2ef96b8af6
swathik1996 <145492237+swathik1996@users.noreply.github.com>
2025-04-29 12:48:29 +0530

    Update entity.tf

--- entity.tf
+++ entity.tf
@@ -45,19 +45,37 @@ resource "google_dialogflow_cx_entity_type" "basic_entity_type" {
 
 
 locals {
-  entity_json_files = fileset("${var.user_folder}/entityTypes", "**/*.json")
+  entity_types_path = "${var.user_folder}/entityTypes"
 
+  # List all en.json files under entityTypes (recursively)
+  entity_json_files = fileset(local.entity_types_path, "**/entities/en.json")
+
+  # Extract the parent entity folder from path like "branch/entities/en.json"
   entity_folders = [
-    dirname(dirname(file))  # extracts 'branch' from 'entityTypes/branch/branch.json'
+    split("/", file)[0]
     for file in local.entity_json_files
-    if file =~ ".*\\/entities\\/en\\.json"
   ]
 
-  unique_folders = toset(local.entity_folders)
-
+  # Build the entity list using decoded JSON from each folder
   entities_data = [
-    for folder in local.unique_folders : {
-      entity_info   = jsondecode(file("${var.user_folder}/entityTypes/${folder}/${folder}.json"))
-      entity_values = jsondecode(file("${var.user_folder}/entityTypes/${folder}/entities/en.json"))
+    for folder in local.entity_folders : {
+      entity_info   = jsondecode(file("${local.entity_types_path}/${folder}/${folder}.json"))
+      entity_values = jsondecode(file("${local.entity_types_path}/${folder}/entities/en.json"))
     }
   ]
+
+  # Final entity list for resource creation
+  entity_list = [
+    for e in local.entities_data : {
+      display_name            = e.entity_info.displayName
+      kind                    = e.entity_info.kind
+      enable_fuzzy_extraction = try(e.entity_info.enable_fuzzy_extraction, false)
+      entities = [
+        for ent in e.entity_values.entities : {
+          value    = ent.value
+          synonyms = ent.synonyms
+        }
+      ]
+    }
+  ]
+}
